d774d7c65669a39472b81032a3289f51864e2185
Choi-InHo <cih1174@gmail.com>
2023-12-28 14:56:59 +0900

    [add] Property Service

--- src/main/java/com/realEstate/realEstate/repository/PropertyRepository.java
+++ src/main/java/com/realEstate/realEstate/repository/PropertyRepository.java
@@ -4,6 +4,8 @@ package com.realEstate.realEstate.repository;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import com.realEstate.realEstate.model.entity.Property;
 import com.realEstate.realEstate.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,5 @@ import java.util.Optional;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Integer> {
-
-    Optional<Property> findByName(String userName);
+    public Page<Property> findAllByUser(User user, Pageable pageable);
 }
