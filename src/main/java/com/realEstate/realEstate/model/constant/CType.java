aad8989107fce6d20cb12ed1fe24f6c9e6e3885d
hahaharin <harin2950@naver.com>
2023-12-29 22:26:07 +0900

    [fix] enum 규칙

--- src/main/java/com/realEstate/realEstate/model/constant/CType.java
+++ src/main/java/com/realEstate/realEstate/model/constant/CType.java
@@ -2,8 +2,8 @@ package com.realEstate.realEstate.model.constant;
 
 public enum CType {
     // 부동산 계약 형태
-    Sale,//매매
-    Jeonse, // 전세
-    Month_rent // 월세
+    SALE,//매매
+    JEONSE, // 전세
 
+    MONTHLY_RENT // 월세
 }
