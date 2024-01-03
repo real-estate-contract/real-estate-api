e3f513b67063f95fa8a726392b3e19ffb597f94c
Choi-InHo <cih1174@gmail.com>
2024-01-03 16:21:28 +0900

    test

--- src/main/java/com/realEstate/realEstate/model/entity/User.java
+++ src/main/java/com/realEstate/realEstate/model/entity/User.java
@@ -22,6 +22,7 @@ public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer userId;
 
     @Column(nullable = false)
