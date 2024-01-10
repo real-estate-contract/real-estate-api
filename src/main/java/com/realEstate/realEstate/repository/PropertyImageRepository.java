277fdc0be7eff2ee38cd3ddb871033efd3a8728b
Choi-InHo <cih1174@gmail.com>
2024-01-10 18:11:25 +0900

    [fix] 수정

--- src/main/java/com/realEstate/realEstate/repository/PropertyImageRepository.java
+++ src/main/java/com/realEstate/realEstate/repository/PropertyImageRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 
 @Repository
 public interface PropertyImageRepository extends JpaRepository<PropertyImage, Long> {
-        List<PropertyImage> findByPropertyId(Long propertyId);
 }
