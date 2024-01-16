820fdccaca8c4eff15fb293353e4b46680359c37
hahaharin <harin2950@naver.com>
2024-01-16 17:11:33 +0900

    [fix] 충돌 해결

--- src/main/java/com/realEstate/realEstate/repository/ContractRepository.java
+++ src/main/java/com/realEstate/realEstate/repository/ContractRepository.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     // contractId로 사용자의 계약을 페이지로 조회
-    public Page<Contract> findById(Long id, Pageable pageable);
+//    public Page<Contract> findById(Long id, Pageable pageable);
+    Optional<Contract> findById(Long id);
 
 }
