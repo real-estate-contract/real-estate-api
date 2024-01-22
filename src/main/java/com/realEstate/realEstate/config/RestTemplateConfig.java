497bc10268405144b6dc65308f56ce1c8f74f8a1
Choi-InHo <cih1174@gmail.com>
2024-01-22 19:14:36 +0900

    [add] chatgpt

--- src/main/java/com/realEstate/realEstate/config/RestTemplateConfig.java
+++ src/main/java/com/realEstate/realEstate/config/RestTemplateConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
-public class RestTemplateConfig {
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
-    }
-}
+//@Configuration
+//public class RestTemplateConfig {
+//
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+//        return restTemplateBuilder.build();
+//    }
+//}
---
+++ src/main/java/com/realEstate/realEstate/controller/ChatGPTController.java
@@ -0,0 +1,54 @@
+package com.realEstate.realEstate.controller;
+
+import com.realEstate.realEstate.model.dto.ChatGpt.ChatCompletionDto;
+import com.realEstate.realEstate.model.dto.ChatGpt.CompletionDto;
+import com.realEstate.realEstate.service.ChatGpt.ChatGPTService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/v1/chatGpt")
+public class ChatGPTController {
+
+    private final ChatGPTService chatGPTService;
+
+    public ChatGPTController(ChatGPTService chatGPTService) {
+        this.chatGPTService = chatGPTService;
+    }
+
+    @GetMapping("/modelList")
+    public ResponseEntity<List<Map<String, Object>>> selectModelList() {
+        List<Map<String, Object>> result = chatGPTService.modelList();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/model")
+    public ResponseEntity<Map<String, Object>> isValidModel(@RequestParam(name = "modelName") String modelName) {
+        Map<String, Object> result = chatGPTService.isValidModel(modelName);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/legacyPrompt")
+    public ResponseEntity<Map<String, Object>> selectLegacyPrompt(@RequestBody CompletionDto completionDto) {
+        log.debug("param :: " + completionDto.toString());
+        Map<String, Object> result = chatGPTService.legacyPrompt(completionDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("/prompt")
+    public ResponseEntity<Map<String, Object>> selectPrompt(@RequestBody ChatCompletionDto chatCompletionDto) {
+        log.debug("param :: " + chatCompletionDto.toString());
+        Map<String, Object> result = chatGPTService.prompt(chatCompletionDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
+}
