a773ff2f8f8624bd9cd6781fc936febdfda8d963
Choi-InHo <cih1174@gmail.com>
2024-04-14 17:47:48 +0900

    dsas

---
+++ src/main/java/com/realEstate/realEstate/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.realEstate.realEstate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:3000","http://3.34.191.254")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
