699c3b6ee48bbc9c8da5115882b39d25fb3958ab
harin1212 <harin2950@naver.com>
2024-06-28 10:46:10 +0900

    [add] CORS

--- src/main/java/com/realEstate/realEstate/config/WebSecurityConfig.java
+++ src/main/java/com/realEstate/realEstate/config/WebSecurityConfig.java
@@ -12,7 +12,8 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOrigins("http://localhost:3000","http://3.34.191.254")
+                .allowedOrigins("http://localhost:3000","http://3.34.191.254", "http://budong.com.s3-website.ap-northeast-2.amazonaws.com/",
+                        "http://budong.com", "https://budong.com")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
